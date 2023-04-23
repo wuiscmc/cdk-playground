9137375c9519650de3ef5052ca6d062f2cc27f4b
Carlos Mateos <carlos.mateos@viaplaygroup.com>
2023-04-23 23:45:17 +0200

    Initial commit

---
+++ test/custom-resource-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CustomResourceCdk from '../lib/custom-resource-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/custom-resource-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CustomResourceCdk.CustomResourceCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
