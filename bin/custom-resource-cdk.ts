dc02c0aa1cf8013123d29855817a81607b3ceb5b
Carlos Mateos <carlos.mateos@viaplaygroup.com>
2023-04-24 23:40:02 +0200

    test codedeploy

--- bin/custom-resource-cdk.ts
+++ bin/custom-resource-cdk.ts
@@ -2,11 +2,12 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CustomResourceCdkStack } from '../lib/custom-resource-cdk-stack';
-import { MyGitHubActionRole } from '../lib/github-action-role-stack';
+// import { MyGitHubActionRole } from '../lib/github-action-role-stack';
 import config from '../config.json';
-import { AwsCredentials, GitHubWorkflow } from 'cdk-pipelines-github';
-import { ShellStep } from 'aws-cdk-lib/pipelines';
+// import { AwsCredentials, GitHubWorkflow } from 'cdk-pipelines-github';
+import * as pipelines from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { Stack, StackProps, Stage } from 'aws-cdk-lib';
 
 const app = new cdk.App();
 
@@ -17,7 +18,7 @@ const app = new cdk.App();
 //   }
 // });
 
-class MyStage extends cdk.Stage {
+class MyStage extends Stage {
   constructor(scope: Construct, id: string, props?: cdk.StageProps) {
     super(scope, id, props);
 
@@ -25,25 +26,64 @@ class MyStage extends cdk.Stage {
   }
 }
 
-const pipeline = new GitHubWorkflow(app, 'Pipeline', {
-  publishAssetsAuthRegion: config['aws.region'],
-  synth: new ShellStep('Build', {    
-    commands: [
-      'npm install',
-      'npm run build',
-      'npx cdk synth',
-    ],
-  }),
-  awsCreds: AwsCredentials.fromOpenIdConnect({
-    gitHubActionRoleArn: config['aws.github-role.arn'],
-  }),
-});
+class MyPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-const prodStage = new MyStage(app, 'Prod', {
+    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+      crossAccountKeys: true,
+      synth: new pipelines.ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: pipelines.CodePipelineSource.connection('wuiscmc/cdk-playground', 'main', {
+          connectionArn: 'arn:aws:codestar-connections:eu-north-1:552193173995:connection/ebc5a7d0-f606-436a-9467-55b84adfd416', // Created using the AWS console * });',
+        }),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });
+
+    pipeline.addStage(
+      new MyStage(app, 'Prod', {
+        env: {
+          account: '682444753419', //sandbox, // config['aws.deployment.accountId'],
+          region: config['aws.region']
+        }
+      })
+    );
+  }
+}
+
+new MyPipelineStack(app, 'PipelineStack', {
   env: {
-    account: config['aws.deployment.accountId'], 
-    region: config['aws.region']
+    account: config['aws.deployment.accountId'],
+    region: config['aws.region'],
   }
 });
 
-pipeline.addStage(prodStage);
+
+// const pipeline = new GitHubWorkflow(app, 'Pipeline', {
+//   publishAssetsAuthRegion: config['aws.region'],
+//   synth: new ShellStep('Build', {    
+//     commands: [
+//       'npm install',
+//       'npm run build',
+//       'npx cdk synth',
+//     ],
+//   }),
+//   awsCreds: AwsCredentials.fromOpenIdConnect({
+//     gitHubActionRoleArn: config['aws.github-role.arn'],
+//   }),
+// });
+
+// const prodStage = new MyStage(app, 'Prod', {
+//   env: {
+//     account: config['aws.deployment.accountId'], 
+//     region: config['aws.region']
+//   }
+// });
+
+// pipeline.addStage(prodStage);
